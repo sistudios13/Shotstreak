dc3feb0e37487bde3bca0abe04698c8cbda77482
sistudios13 <si.studios13@gmail.com>
2025-01-09 13:12:08 -0500

    Finished Navbars

--- playerprofile.php
+++ playerprofile.php
@@ -170,20 +170,32 @@ while ($brow = $bresult_chart->fetch_assoc()) {
 <body class="bg-lightgray dark:bg-almostblack min-h-screen">
 
     <!-- Navbar -->
-    <nav id="test" class="bg-white dark:bg-darkslate shadow-md py-2 md:py-4">
-        <div class="container mx-auto flex justify-between items-center px-6">
-            <a href="index.php" class="text-2xl font-bold text-coral">
-                <img src="assets/isoLogo.svg" class="size-12 md:hidden" alt="logo">
-                <span class="hidden md:block">Shotstreak</span>
+<header id="navbar" class="sticky shadow-md bg-white dark:bg-darkslate  top-0 w-full z-20">
+        <nav class="flex justify-between lg:container mx-auto px-4 lg:px-6 py-3 lg:py-0 " x-data="{isOpen : false, current: 1}" @click.outside="() => { if(window.innerWidth < 1024) {isOpen = false} }" x-init="if(window.innerWidth >= 1024) {isOpen = true}">
+            <a href="index.php" class="text-2xl font-semibold text-coral">
+                <img src="assets/isoLogo.svg" class="size-12 lg:size-14 lg:my-2" alt="Shotstreak">
             </a>
-            <div class="flex items-center gap-2">
-                <button id="theme-toggle"><img class="size-5 dark:hidden" src="assets/dark.svg" alt="dark"><img class="size-5 hidden dark:block" src="assets/light.svg" alt="dark"></button>
-                
-                <a href="coach_dashboard.php" class="text-almostblack dark:text-lightgray md:hover:text-coral">Dashboard</a>
-                <a href="logout.php" class="text-almostblack dark:text-lightgray md:hover:text-coral">Logout</a>
+            <!-- Menu Button -->
+            <div id="bars" class="flex items-center lg:hidden">
+                <button @click="isOpen = !isOpen" class="flex flex-col gap-1 items-center px-3 pr-0 py-2 text-gray-500 border-0 rounded">
+                    <div id="bar1" class="w-5 rounded h-0.5 bg-almostblack dark:bg-lightgray transition-all" x-bind:class="{ '-rotate-45 translate-y-1.5 bg-coral dark:bg-coral': isOpen }"></div>
+                    <div id="bar1" class="w-5 rounded h-0.5 bg-almostblack dark:bg-lightgray transition-all" x-bind:class="{ 'opacity-0': isOpen }"></div>
+                    <div id="bar1" class="w-5 rounded h-0.5 bg-almostblack dark:bg-lightgray transition-all" x-bind:class="{ 'rotate-45 -translate-y-1.5 bg-coral dark:bg-coral': isOpen }"></div>
+                </button>
             </div>
-        </div>
-    </nav>
+            <ul class="absolute shadow-md mt-[70px] lg:py-3 text-almostblack dark:text-lightgray bg-white dark:bg-darkslate pb-8 flex-col items-end flex w-full lg:static top-0 right-0 p-4 lg:text-lg float-right gap-4 lg:p-0 lg:justify-end lg:items-center lg:flex-row lg:shadow-none lg:mt-0 text-xl" x-show="isOpen" x-cloak x-transition:enter="transition ease-out duration-200"
+                    x-transition:enter-start="opacity-0  translate-x-12"
+                    x-transition:enter-end="opacity-100 scale-100"
+                    x-transition:leave="transition ease-in duration-75"
+                    x-transition:leave-start="opacity-100 scale-100"
+                    x-transition:leave-end="opacity-0 translate-x-4">
+                <li><a href="index.php" class="cursor-pointer w-full text-right lg:hover:text-coral">Dashboard</a></li>
+                <li><a href="coachprofile.php" class="cursor-pointer lg:hover:text-coral">Profile</a></li>
+                <li><a href="logout.php" class="cursor-pointer lg:hover:text-coral">Logout</a></li>
+                <li class="h-[24px]"><button id="theme-toggle"><img class="size-6 dark:hidden" src="assets/dark.svg" alt="dark"><img class="size-6 hidden dark:block" src="assets/light.svg" alt="dark"></button></li>
+            </ul>
+        </nav>
+    </header> 
 
     <!-- Main Content -->
     <div class="container mx-auto px-6 py-8">
